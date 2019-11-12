9a858f78d7d50812663236466473a982afa18ae3
Camiel Bartelet <c.bartelet@student.fontys.nl>
2019-11-12 16:56:12 +0100

    Reference and scenes

---
+++ Scripts/lefttoright.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lefttoright : MonoBehaviour
+{
+
+public float speed = 0.8f;
+Vector3 pointA;
+Vector3 pointB;
+float currentLerpTime;
+private bool _isLerping;
+
+void Start()
+{
+    pointA = new Vector3(0, 0, 0);
+    pointB = new Vector3(5, 0, 0);
+    
+}
+
+void GetItOn(){
+    currentLerpTime = Time.time;
+    _isLerping = true;
+}
+
+void FixedUpdate()
+{   if(Input.GetKeyDown("space")){
+        GetItOn();
+    }
+    //PingPong between 0 and 1
+    if(_isLerping){
+    float timeSinceLerp = Time.time - currentLerpTime;
+    float t = timeSinceLerp/speed;
+                        
+    //float Perc = t*t*t * (t * (6f*t - 15f) + 10f);
+    float time = Mathf.PingPong(t, 1);
+    transform.position = Vector3.Lerp(pointA, pointB, time);
+
+    if(t>=2.5f && transform.position == pointA)
+            {
+                _isLerping = false;
+            }
+        }
+    
+
+}
+
+}
